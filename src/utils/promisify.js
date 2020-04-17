dd26507df218fed9e5638d2bde7da07e27abb512
youboli <youboli@yangqianguan.com>
2020-04-17 17:38:07 +0800

    [promisify] 将回调函数模式的 API 转为 Promise

---
+++ src/utils/promisify.js
@@ -0,0 +1,13 @@
+function promisify(fn) {
+  return function(...args) {
+    return new Promise((resolve, reject) => {
+      args.push((err, data) => {
+        if (err) reject(err)
+        else resolve(data)
+      })
+      fn.apply(null, args)
+    })
+  }
+}
+
+module.exports = promisify
